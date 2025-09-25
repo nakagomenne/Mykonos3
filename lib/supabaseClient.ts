2073fbfc8f0c9fb929cb8f76a8fbab6bf6a1132c
nakagomenne <k.nakagome@nnegrp.co.jp>
2025-09-25 13:41:22 +0900

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+// lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL!,rpcfucwdeofghxwxfhwb
+  import.meta.env.VITE_SUPABASE_ANON_KEY!eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwY2Z1Y3dkZW9mZ2h4d3hmaHdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5ODkyMDgsImV4cCI6MjA3MzU2NTIwOH0.kjaPd7FA0FRNMh3bfaZYrAufO4JJ4NbCW4ONmQOgqA0
+);
